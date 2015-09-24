e19b35282d33df3d5c1023649fbbea90b0554013
Aksel Meola <aksel@meola.eu>
2015-09-24 19:56:16 +0300

    Code cleanups

--- mainav.rb
+++ mainav.rb
@@ -30,9 +30,9 @@ module Jekyll
     #
     # Initilize class
     #
-    def initialize(tag_name, text, tokens)
+    def initialize(tag_name, args, tokens)
       super
-      @text = text
+      @classnames = args
     end
  
     #
@@ -41,16 +41,15 @@ module Jekyll
     def render(context)
 
       site = context.registers[:site]
-
       current_page = context.registers[:page]
+      
       pages = pages_with_category(site.pages, current_page["category"])
       
 			if pages.length != 0
 				items = render_menu(pages, pages.select{|page| page_id_length( page ) == 1 } )
 			end
 	    
-
-      %( <ul class="#{@text}">#{items}</ul> )
+      %( <ul class="#{@classnames}">#{items}</ul> )
 
     end
  
@@ -70,6 +69,7 @@ module Jekyll
 	    #
 	    # Get pages's ID part
 	    #
+	    #
 	    def get_page_id(page)
 	    	page.path.split("/").last.split(@@id_delimiter).first
 	    end
@@ -81,13 +81,30 @@ module Jekyll
 				get_page_id(page_id).split(@@delimiter).length
 			end
 
+			#
+			# Get the first part of the page id
+			#
+			#
 			def group_of(page_id)
 				 get_page_id(page_id).split(@@delimiter).first
 			end
 
-			def get_subitems(page_ids, cur_id)
-				subitems = page_ids.select{|id| 
-					group_of(id) == group_of(cur_id) and page_id_length(id) == page_id_length(cur_id)+1 and ( get_page_id(id).casecmp get_page_id(cur_id) )>0 }		
+			# 
+			# Get all pages that are a direct subpages of the current page.
+			#
+			# Page is a subpage if:
+			#  * Starts's with a same level/group number
+			#  * Has more levels in it's id
+			#  * ID is alphabetically after current page
+			# 
+			def get_subitems(pages, cur_page)
+				
+				pages.select{|page| 
+					group_of(page) == group_of(cur_page) and
+					page_id_length(page) == page_id_length(cur_page) + 1 and
+					( get_page_id(page).casecmp get_page_id(cur_page) ) > 0 
+				}		
+
 			end
 
 
@@ -96,17 +113,16 @@ module Jekyll
 			#
 			def render_menu(pages, page_ids)
 
-				output = ""
+				html = ""
 
 				for i in 0..page_ids.length-1
 
 					subitems = get_subitems(pages, page_ids[i] );
 
 					if subitems.length == 0 
-						o <<  %(<li><a href="#{page_ids[i].url}">#{page_ids[i].data["title"]}</a></li> )
-
+						html <<  %(<li><a href="#{page_ids[i].url}">#{page_ids[i].data["title"]}</a></li> )
 					else
-						o << %(
+						html << %(
 							<li>
 							<a href="#{page_ids[i].url}">#{page_ids[i].data["title"]}</a> 
 								<ul>
@@ -117,7 +133,7 @@ module Jekyll
 
 				end
 
-				return o
+				return html
 
 			end
 	 
