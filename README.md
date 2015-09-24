be47b975c85cd7a8456cb6b6627c7629e01bb15b
Aksel Meola <aksel@meola.eu>
2015-09-25 00:07:57 +0300

    Cleanups. Changed tag name to catnav to be more suitable for new feature - generate navigation for given category. By default current pages category is used.

--- README.md
+++ README.md
@@ -13,7 +13,25 @@ Copy the mainav.rb to your Jekyll sites _plugins directory. If the _plugins dire
 
 ## Usage
 
-PLace liquid tag `{% page_navigation navclass %}` into your navigation area.
+Place liquid tag `{% catnav %}` into your navigation area.
 
 Now to make pages nestable, add each page you want in menu to same category by inserting `category: "mycat"` 
 to each page's frontmatter and name your pages like this: `1-page, 1.1-some-subpage, 1.1.1-even-more-subpage ... etc`
+
+No menu is generated when category is not set in frontmatter.
+
+## Tag attributes
+
+### category:
+You can also use catnav tag as a table of contents generator by placing tag at the beginning of the page and use a parameter category: <category name> to generate navigation for specific category. By default current pages category is used. 
+        
+        {% catnav category: myothercat %}    
+
+### class:
+Class names for stylesheet.
+
+        {% catnav class: "toc nav blue-box" %}    
+
+
+
+
--- mainav.rb
+++ mainav.rb
@@ -12,13 +12,13 @@
 #
 # 
 =begin
-	
-	TODO:
-		
-		*	Clean code, replace variable names with better ones
-		* Make delimiter and id delimiter load from config file
-		* Add classes to generated HTML for user styles
-		*
+  
+  TODO:
+    
+    * Clean code, replace variable names with better ones
+    * Make delimiter and id delimiter load from config file
+    * Add classes to generated HTML for user styles
+    *
 
 =end
 
@@ -29,22 +29,53 @@ module Jekyll
     attr_reader :dir
   end
  
-  class PageNavigationTag < Liquid::Tag
+  #
+  # Category navigation
+  # 
+  # Generate nesting navigation menu out of pages in same category.
+  # By default current page's category is chosen but this can be overrided by adding a
+  # category parameter to the tag. 
+  #
+  # 
+  class CatNavTag < Liquid::Tag
 
-  	#
-  	# Page delimiter
-  	#
-  	@@delimiter = "."
-  	@@id_delimiter = "-"
+
+    #
+    # Tag to use in liquid templates to call category navigation
+    #
+    TAG_NAME = 'catnav'
+
+    #
+    # Page delimiter
+    #
+    @@delimiter = "."
+    @@id_delimiter = "-"
 
     #
     # Initilize class
     #
     def initialize(tag_name, args, tokens)
       super
-      @classnames = args
-    end
  
+      @classnames = nil
+      @category   = nil
+
+      # Scan for tag arguments
+      args.scan(Liquid::TagAttributes) do |key, value|  
+        
+        if key == "class"
+          @classnames = value.tr("\"'", "");
+        end
+
+        if key == "category"
+          @category = value.tr("\"'", "");
+        end
+          
+      end
+
+
+    end
+
     #
     # Render naviation menu
     #
@@ -53,13 +84,13 @@ module Jekyll
       site = context.registers[:site]
       current_page = context.registers[:page]
       
-      pages = pages_with_category(site.pages, current_page["category"])
+      pages = pages_with_category(site.pages, (@category || current_page["category"]))
       
-			if pages.length != 0
-				items = render_menu(pages, pages.select{|page| page_id_length( page ) == 1 } )
-			end
-	    
-      return %( <ul class="#{@classnames}">#{items}</ul> )
+      if pages.length != 0
+        items = render_menu(pages, pages.select{|page| page_id_length( page ) == 1 } )
+      end
+
+      return %( <ul class="#{(@classnames || "")}">#{items}</ul> )
 
     end
  
@@ -67,91 +98,94 @@ module Jekyll
 
     private 
 
-	    #
-	    # Get pages that are in the same category as the current page
-	    #
-	    # TODO: Make it search in categories also
-	    def pages_with_category(pages, category)
-	      return pages.select { |page|
-	        !page.data["category"].nil? && page.data["category"] == category }
-	    end
+      #
+      # Get pages that are in the same category as the current page
+      #
+      # TODO: Make it search in categories also
+      def pages_with_category(pages, category)
+        return pages.select { |page|
+          !page.data["category"].nil? && page.data["category"] == category }
+      end
 
-	    #
-	    # Get pages's ID part
-	    #
-	    def get_page_id(page)
-	    	return page.path.split("/").last.split(@@id_delimiter).first
-	    end
+      #
+      # Get pages's ID part
+      #
+      def get_page_id(page)
+        return page.path.split("/").last.split(@@id_delimiter).first
+      end
 
-	    #
-	    # Get how many levels in page id
-	    #
-			def page_id_length(page)
-				return get_page_id(page).split(@@delimiter).length
-			end
+      #
+      # Get how many levels in page id
+      #
+      def page_id_length(page)
+        return get_page_id(page).split(@@delimiter).length
+      end
 
-			#
-			# Get the first part of the page id
-			#
-			def group_of(page)
-				 return get_page_id(page).split(@@delimiter).first
-			end
+      #
+      # Get the first part of the page id
+      #
+      def group_of(page)
+         return get_page_id(page).split(@@delimiter).first
+      end
 
-			# 
-			# Get all pages that are a direct subpages of the current page.
-			#
-			# Page is a subpage if:
-			#  * Starts's with a same level/group number
-			#  * Has more levels in it's id
-			#  * ID is alphabetically after current page
-			# 
-			def get_subitems(pages, cur_page)
-				
-				return pages.select{|page| 
-					group_of(page) == group_of(cur_page) and
-					page_id_length(page) == page_id_length(cur_page) + 1 and
-					( get_page_id(page).casecmp get_page_id(cur_page) ) > 0 
-				}		
+      # 
+      # Get all pages that are a direct subpages of the current page.
+      #
+      # Page is a subpage if:
+      #  * Starts's with a same level/group number
+      #  * Has more levels in it's id
+      #  * ID is alphabetically after current page
+      # 
+      def get_subitems(pages, cur_page)
+        
+        return pages.select{|page| 
+          group_of(page) == group_of(cur_page) and
+          page_id_length(page) == page_id_length(cur_page) + 1 and
+          ( get_page_id(page).casecmp get_page_id(cur_page) ) > 0 
+        }   
 
-			end
+      end
 
 
-			#
-			# Render menu
-			# 
-			# Recursively parse the pages tree and 
-			# generate nested HTML lists.
-			#
-			def render_menu(pages, cur_pages)
+      #
+      # Render menu
+      # 
+      # Recursively parse the pages tree and 
+      # generate nested HTML lists.
+      #
+      def render_menu(pages, cur_pages)
 
-				html = ""
+        html = ""
 
-				for i in 0..cur_pages.length-1
+        for i in 0..cur_pages.length-1
 
-					subpages = get_subitems(pages, cur_pages[i] );
+          subpages = get_subitems(pages, cur_pages[i] );
 
-					if subpages.length == 0 
-						html <<  %(<li><a href="#{cur_pages[i].url}">#{cur_pages[i].data["title"]}</a></li> )
-					else
-						html << %(
-							<li>
-							<a href="#{cur_pages[i].url}">#{cur_pages[i].data["title"]}</a> 
-								<ul>
-									#{ render_menu(pages, subpages) }
-								</ul>
-							</li> )
-					end
+          if subpages.length == 0 
+            html <<  %(<li><a href="#{cur_pages[i].url}">#{cur_pages[i].data["title"]}</a></li> )
+          else
+            html << %(
+              <li>
+              <a href="#{cur_pages[i].url}">#{cur_pages[i].data["title"]}</a> 
+                <ul>
+                  #{ render_menu(pages, subpages) }
+                </ul>
+              </li> )
+          end
 
-				end
+        end
 
-				return html
+        return html
 
-			end
-	 
+      end
+   
 
 
   end
+
 end
- 
-Liquid::Template.register_tag('page_navigation', Jekyll::PageNavigationTag)
+
+# Register catnav tag
+# 
+Liquid::Template.register_tag( Jekyll::CatNavTag::TAG_NAME, Jekyll::CatNavTag)
 
